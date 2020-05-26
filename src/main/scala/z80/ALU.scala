55861bfb4b0fcf44d4f09fc15ecb1637cf00d19b
Joshua Bassett <hello@joshbassett.info>
2020-05-26 16:10:17 +1000

    Set the half-carry flag

--- src/main/scala/z80/ALU.scala
+++ src/main/scala/z80/ALU.scala
@@ -7,7 +7,7 @@ import chisel3.util._
  * Operations
  */
 object Ops {
-  val nop :: add :: sub :: Nil = Enum(3)
+  val add :: sub :: Nil = Enum(2)
 }
 
 class Flags extends Bundle {
@@ -17,7 +17,7 @@ class Flags extends Bundle {
   val half = UInt(1.W)
   val unused1 = UInt(1.W)
   val parity = UInt(1.W)
-  val negative = UInt(1.W)
+  val subtract = UInt(1.W)
   val carry = UInt(1.W)
 }
 
@@ -52,27 +52,29 @@ class ALU extends Module {
   // default value
   val result = WireDefault(0.U(9.W))
 
-  switch (io.op) {
-    is (Ops.nop) {
-      result := io.b
-    }
-    is (Ops.add) {
-      result := io.a +& io.b
-    }
-    is (Ops.sub) {
-      result := io.a -& io.b
-    }
-  }
-
   val flags = Wire(new Flags)
-  flags.carry := result(8)
-  flags.negative := 0.U
-  flags.parity := 0.U
+  flags.sign := 0.U
+  flags.zero := result(7, 0) === 0.U
   flags.unused1 := 0.U
   flags.half := 0.U
   flags.unused2 := 0.U
-  flags.zero := result(7, 0) === 0.U
-  flags.sign := 0.U
+  flags.parity := 0.U
+  flags.subtract := 0.U
+  flags.carry := result(8)
+
+  io.q := 0.U
+
+  switch (io.op) {
+    is (Ops.add) {
+      result := io.a +& io.b
+      flags.half := (io.a(3, 0) +& io.b(3, 0))(4)
+    }
+    is (Ops.sub) {
+      result := io.a -& io.b
+      flags.half := (io.a(3, 0) -& io.b(3, 0))(4)
+      flags.subtract := 1.U
+    }
+  }
 
   io.q := result(7, 0)
   io.flags := flags.asUInt()
--- src/test/scala/z80/ALUTest.scala
+++ src/test/scala/z80/ALUTest.scala
@@ -5,18 +5,6 @@ import chiseltest._
 import org.scalatest._
 
 class ALUTest extends FlatSpec with ChiselScalatestTester with Matchers {
-  behavior of "NOP"
-
-  it should "output B" in {
-    test(new ALU) { c =>
-      c.io.op.poke(Ops.nop)
-      c.io.a.poke(2.U)
-      c.io.b.poke(1.U)
-      c.io.q.expect(1.U)
-      c.io.flags.expect("b0000_0000".U)
-    }
-  }
-
   behavior of "ADD"
 
   it should "output A + B" in {
@@ -39,13 +27,23 @@ class ALUTest extends FlatSpec with ChiselScalatestTester with Matchers {
     }
   }
 
+  it should "set the half-carry flag" in {
+    test(new ALU) { c =>
+      c.io.op.poke(Ops.add)
+      c.io.a.poke(15.U)
+      c.io.b.poke(1.U)
+      c.io.q.expect(16.U)
+      c.io.flags.expect("b0001_0000".U)
+    }
+  }
+
   it should "set the carry flag" in {
     test(new ALU) { c =>
       c.io.op.poke(Ops.add)
       c.io.a.poke(255.U)
       c.io.b.poke(2.U)
       c.io.q.expect(1.U)
-      c.io.flags.expect("b0000_0001".U)
+      c.io.flags.expect("b0001_0001".U)
     }
   }
 
@@ -57,7 +55,7 @@ class ALUTest extends FlatSpec with ChiselScalatestTester with Matchers {
       c.io.a.poke(2.U)
       c.io.b.poke(1.U)
       c.io.q.expect(1.U)
-      c.io.flags.expect("b0000_0000".U)
+      c.io.flags.expect("b0000_0010".U)
     }
   }
 
@@ -67,7 +65,17 @@ class ALUTest extends FlatSpec with ChiselScalatestTester with Matchers {
       c.io.a.poke(1.U)
       c.io.b.poke(1.U)
       c.io.q.expect(0.U)
-      c.io.flags.expect("b0100_0000".U)
+      c.io.flags.expect("b0100_0010".U)
+    }
+  }
+
+  it should "set the half-carry flag" in {
+    test(new ALU) { c =>
+      c.io.op.poke(Ops.sub)
+      c.io.a.poke(16.U)
+      c.io.b.poke(1.U)
+      c.io.q.expect(15.U)
+      c.io.flags.expect("b0001_0010".U)
     }
   }
 
@@ -77,7 +85,7 @@ class ALUTest extends FlatSpec with ChiselScalatestTester with Matchers {
       c.io.a.poke(0.U)
       c.io.b.poke(1.U)
       c.io.q.expect(255.U)
-      c.io.flags.expect("b0000_0001".U)
+      c.io.flags.expect("b0001_0011".U)
     }
   }
 }
