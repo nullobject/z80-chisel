60d57eab0259a7448296de78d5ddacce26e55e96
Joshua Bassett <hello@joshbassett.info>
2020-05-26 10:43:57 +1000

    Add ALU operations

--- src/test/scala/ALUTest.scala
+++ src/test/scala/ALUTest.scala
@@ -3,11 +3,21 @@ import chiseltest._
 import chisel3._
 
 class ALUTest extends FlatSpec with ChiselScalatestTester with Matchers {
-  it should "do something" in {
+  it should "ADD" in {
     test(new ALU) { c =>
-      c.io.a.poke(1.U)
-      c.io.b.poke(2.U)
-      c.io.c.expect(3.U)
+      c.io.a.poke(2.U)
+      c.io.b.poke(1.U)
+      c.io.op.poke(Ops.add)
+      c.io.q.expect(3.U)
+    }
+  }
+
+  it should "SUB" in {
+    test(new ALU) { c =>
+      c.io.a.poke(2.U)
+      c.io.b.poke(1.U)
+      c.io.op.poke(Ops.sub)
+      c.io.q.expect(1.U)
     }
   }
 }
